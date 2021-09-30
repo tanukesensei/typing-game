f01d70a2448189bdb7acf40dba663478f15c08ae
tanukesensei <luanvs.hack@gmail.com>
2021-09-29 23:12:56 -0300

    first typing-game commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import wordList from "./resources/words.json";
 
-function App() {
+const MAX_TYPED_KEYS = 30;
+
+const getWord = () => {
+  const index = Math.floor(Math.random() * wordList.length);
+  const word = wordList[index];
+  return word.toLowerCase();
+};
+
+const isValidKey = (key, word) => {
+  if (!word) return false;
+  const result = word.split("").includes(key);
+  return result;
+};
+
+const Word = ({ word, validKeys }) => {
+  if (!word) return null;
+
+  const joinedKeys = validKeys.join("");
+  const matched = word.slice(0, joinedKeys.length);
+  const remainder = word.slice(joinedKeys.length);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <span className="matched">{matched}</span>
+      <span className="remainder">{remainder}</span>
+    </>
+  );
+};
+
+const App = () => {
+  const [typedKeys, setTypedKeys] = useState([]);
+  const [validKeys, setValidKeys] = useState([]);
+  const [word, setWord] = useState("");
+
+  useEffect(() => {
+    setWord(getWord());
+  }, []);
+
+  const handleKeyDown = (event) => {
+    event.preventDefault();
+    const { key } = event;
+    setTypedKeys((prev) => [...prev, key].slice(MAX_TYPED_KEYS * -1));
+
+    if (isValidKey(key, word)) {
+      setValidKeys((prev) => {
+        const isValidLength = prev.length <= word.length;
+        const isNextChar = isValidLength && word[prev.length] === key;
+        return isNextChar ? [...prev, key] : prev;
+      });
+    }
+  };
+
+  return (
+    <div className="container" onKeyDown={handleKeyDown} tabIndex="0">
+      <div className="valid-keys">
+        <Word word={word} validKeys={validKeys} />
+      </div>
+      <div className="typed-keys">{typedKeys ? typedKeys.join(" ") : null}</div>
+      <div className="completed-words">
+        <ol>
+          <li>Carro</li>
+          <li>Cidade</li>
+          <li>Arbusto</li>
+        </ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
